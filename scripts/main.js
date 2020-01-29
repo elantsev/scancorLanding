6729ef784eede5730e8f52a635c3e1ad7fc60942
elantsev <elantsev.pa@gmail.com>
2020-01-29 17:49:44 +0300

    .

--- scripts/main.js
+++ scripts/main.js
@@ -1,76 +1,94 @@
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction () {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
         document.getElementById("hideOnScroll").style.display = "none";
         document.getElementById("showOnScroll").style.display = "flex";
+        document.querySelector('body>header').style.borderBottom = "1px solid #c4c4c4"
+
     } else {
         document.getElementById("hideOnScroll").style.display = "flex";
         document.getElementById("showOnScroll").style.display = "none";
+        document.querySelector('body>header').style.borderBottom = "none"
     }
 }
 
 
 
-// (function () {
+(function () {
 
-//     var slider = function () {
+    var slider = function () {
 
-//         var images = [];
-//         images[0] = "./assets/slides/slide1.png8";
-//         images[1] = "./assets/slides/slide2.png8";
-//         images[2] = "./assets/slides/slide3.png8";
+        var images = [
+            {
+                src: "./assets/slides/slide1.png8",
+                srcset: "./assets/slides/slide1_sm.png8  800w, ./assets/slides/slide1.png8   1024w,  ./assets/slides/slide1@2x.png8  2x",
+            },
+            {
+                src: "./assets/slides/slide2.png8",
+                srcset: "./assets/slides/slide2_sm.png8  800w, ./assets/slides/slide2.png8   1024w,  ./assets/slides/slide2@2x.png8  2x",
+            },
+            {
+                src: "./assets/slides/slide3.png8",
+                srcset: "./assets/slides/slide3_sm.png8  800w, ./assets/slides/slide3.png8   1024w,  ./assets/slides/slide3@2x.png8  2x",
+            }
+        ];
 
-//         var i = 0;
-//         var x = (images.length) - 1;
-//         var int = 3500;
+        var i = 0;
+        var x = (images.length) - 1;
+        var int = 3500;
 
-//         interval = setInterval(showNext, int); // hoist?
+        interval = setInterval(showNext, int);
 
-//         var elements = {
-//             slider: document.querySelector('#slider'),
-//             btn: {
-//                 left: document.querySelector('.btnLeft'),
-//                 right: document.querySelector('.btnRight')
-//             }
-//         }
+        var elements = {
+            slider: document.querySelector('#slider'),
+            btn: {
+                left: document.querySelector('.btnLeft'),
+                right: document.querySelector('.btnRight')
+            }
+        }
 
-//         var startInterval = function () {
-//             interval = setInterval(showNext, int);
-//         }
+        var startInterval = function () {
+            interval = setInterval(showNext, int);
+        }
 
-//         var stopInterval = function () {
-//             clearInterval(interval);
-//         }
+        var stopInterval = function () {
+            clearInterval(interval);
+        }
 
-//         var attachEvents = function () {
-//             elements.btn.left.onclick = function () { showPrevious(); };
-//             elements.btn.right.onclick = function () { showNext(); };
-//             elements.slider.addEventListener("mouseenter", stopInterval);
-//             elements.slider.addEventListener("mouseleave", startInterval);
-//         };
+        var attachEvents = function () {
+            elements.btn.left.onclick = function () { showPrevious(); };
+            elements.btn.right.onclick = function () { showNext(); };
+            elements.btn.left.addEventListener("mouseenter", stopInterval);
+            elements.btn.left.addEventListener("mouseleave", startInterval);
+            elements.btn.right.addEventListener("mouseenter", stopInterval);
+            elements.btn.right.addEventListener("mouseleave", startInterval);
+            elements.slider.addEventListener("mouseenter", stopInterval);
+            elements.slider.addEventListener("mouseleave", startInterval);
+        };
 
-//         var changeImg = function () {
-//             elements.slider.src = images[i];
-//         }
+        var changeImg = function () {
+            elements.slider.src = images[i].src;
+            elements.slider.srcset = images[i].srcset;
+        }
 
-//         var initialize = (function () {
-//             attachEvents();
-//             changeImg(i);
-//         })();
+        var initialize = (function () {
+            attachEvents();
+            changeImg(i);
+        })();
 
-//         var showPrevious = function () {
-//             (i <= 0) ? i = images.length - 1 : i--;
-//             changeImg(i);
-//         };
+        var showPrevious = function () {
+            (i <= 0) ? i = images.length - 1 : i--;
+            changeImg(i);
+        };
 
-//         var showNext = function () {
-//             (i >= x) ? i = 0 : i++;
-//             changeImg(i);
-//         };
+        var showNext = function () {
+            (i >= x) ? i = 0 : i++;
+            changeImg(i);
+        };
 
-//     };
+    };
 
-//     slider();
+    slider();
 
-// })();
+})();
